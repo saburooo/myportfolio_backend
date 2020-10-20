86cfb69b92f1a350b53250978b1bd7735fea9d19
Aki <65touhei@gmail.com>
2020-10-20 12:54:02 +0900

    短く×クラスがあったのでそれで実装

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about_list),
-    path('<int:pk>/', views.about_detail),
+    path('', views.AboutViewSet.as_view()),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,46 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+
 from mysite.models import About
 from mysite.serializers import AboutSerializer
+
+from rest_framework import status, views, generics, viewsets
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+
 
 # Create your views here.
 
 
-@csrf_exempt
-def about_list(request):
-    """
-    すべてのAboutをリストで表示したりする。
-    """
-    if request.method == 'GET':
-        about = About.objects.all()
-        serializer = AboutSerializer(about, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-
-@csrf_exempt
-def about_detail(request, pk):
-    """
-    詳細を書き換えたり削除したりいろいろやる
-    """
-    try:
-        about = About.objects.get(pk=pk)
-    except About.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = AboutSerializer(about)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = AboutSerializer(about, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        About.delete()
-        return HttpResponse(status=204)
+class AboutViewSet(viewsets.ReadOnlyModelViewSet):
+    """ AboutモデルのCRUD用APIクラス、ただし、read only """
+    queryset = About.objects.all()
+    serializer_class = AboutSerializer
